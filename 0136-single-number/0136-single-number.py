36ebd9abfdbfbe5b076064cc845bc0b45b10a7e1
Eugene Jang <105053478+jinchiim@users.noreply.github.com>
2023-09-19 19:54:12 +0900

    Time: 119 ms (83.34%), Space: 18.8 MB (97.26%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,10 @@
-class Solution(object):
-    def singleNumber(self, nums):
-         for i in set(nums):
-            if nums.count(i) == 1:
-                return i
-        
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        count = {}
+        for num in nums:
+            if num not in count:
+                count[num] = 1
+            else:
+                count.pop(num)
+        for key, value in count.items():
+            return key
